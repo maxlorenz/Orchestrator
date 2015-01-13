0b6a28ebf5c86bc3432e5d31fc06fb2303a9537e
Max Lorenz <max-lorenz@gmx.net>
2015-01-13 10:16:18 +0100

    Refactoring, added removal of command

---
+++ Orchestrator_Light/Command.fs
@@ -0,0 +1,19 @@
+﻿module Command
+
+open System.Diagnostics
+
+let Exec command options callback = 
+    let psi = new ProcessStartInfo(command, options)
+    let proc = new Process(StartInfo = psi, EnableRaisingEvents = true)
+
+    psi.UseShellExecute <- false 
+    psi.RedirectStandardOutput <- true
+    psi.RedirectStandardError <- true 
+    psi.CreateNoWindow <- true
+    psi.StandardOutputEncoding <- System.Text.Encoding.GetEncoding(437)
+    
+    proc.OutputDataReceived.Add(callback)
+    proc.Start() |> ignore
+    proc.BeginOutputReadLine()
+    proc.BeginErrorReadLine()
+    proc.WaitForExit()
---
+++ Orchestrator_Light/Database.fs
@@ -0,0 +1,12 @@
+﻿module Database
+
+open System
+
+let DB = @"\\deployment\orchestrator\db.txt"
+let onlyMyComputer (line: string) = line.StartsWith(Environment.MachineName)
+
+let Remove (cmd: string) =
+    let linesToWrite = IO.File.ReadAllLines(DB) |> Seq.filter(fun line -> not(String.Equals(line, cmd)))
+    IO.File.WriteAllLines(DB, linesToWrite)
+
+let GetMyCommands() = IO.File.ReadAllLines(DB) |> Seq.filter(onlyMyComputer)
